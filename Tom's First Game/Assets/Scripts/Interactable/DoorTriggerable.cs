f8b75973bfba788d9dd2d616c6f43e40bc92275f
Welborn <thomaswelborn@ou.edu>
2020-06-20 19:40:38 -0500

    Make Secret Door Functional

--- Tom's First Game/Assets/Scripts/Interactable/DoorTriggerable.cs
+++ Tom's First Game/Assets/Scripts/Interactable/DoorTriggerable.cs
@@ -10,12 +10,34 @@ public class DoorTriggerable : DialogueTree, IInteractable, ITriggerable
     ITriggerCondition _openCondition;
     SpriteRenderer _spriteRenderer;
     private MovableDoorAnimator _animator;
+    BoxCollider2D [] _boxColliders;
+    int _timeRemaining = -1;
+
+    private void Update()
+    {
+        if (_timeRemaining > 0)
+        {
+            _timeRemaining--;
+        }
+        if (_timeRemaining == 0)
+        {
+            OpenSecretDoor();
+        }
+    }
 
     private void Awake()
     {
         _openCondition = GetComponent<ITriggerCondition>();  
         _spriteRenderer = GetComponent<SpriteRenderer>();  
         _animator = GetComponent<MovableDoorAnimator>();
+        _boxColliders = GetComponents<BoxCollider2D>();
+        foreach (BoxCollider2D box in _boxColliders)
+        {
+            if (!box.isTrigger && !(box.size.x == 3))
+            {
+                box.enabled = false;
+            }
+        }
     }
     
     public void InteractedWith() 
@@ -32,11 +54,23 @@ public class DoorTriggerable : DialogueTree, IInteractable, ITriggerable
             {
                 _animator.PlayOpenAnimation();
             }
-            
+            _timeRemaining = 600;
         }
         else 
         {
             _spriteRenderer.sprite = _closedSprite;
+        }
+    }
+
+    public void OpenSecretDoor()
+    {
+        foreach (BoxCollider2D box in _boxColliders)
+        {
+            if (!box.isTrigger)
+            {
+                box.enabled = !box.enabled;
+                _timeRemaining = -1;
+            }
         }
     }
 }
